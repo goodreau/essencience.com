a2420f6ce6886716eef7c87c1b5cf7ab41dddb34
Quintessent.one <gene.goodreau@icloud.com>
2026-01-11 22:18:18 -0500

    Remove Flux CSS; retain CSS-only pipeline (Laravel-only)

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -10,5 +10,4 @@
 <link rel="preconnect" href="https://fonts.bunny.net">
 <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
 
-<link rel="stylesheet" href="{{ asset('flux/flux.css') }}">
 <link rel="stylesheet" href="{{ asset('css/app.css') }}">
