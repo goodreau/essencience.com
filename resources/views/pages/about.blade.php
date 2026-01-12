8c2f1ccbe21a75caf4935ff1c6c3655cae37c266
Quintessent.one <gene.goodreau@icloud.com>
2026-01-11 22:21:19 -0500

    Add static Laravel view for About page (no JS)

---
+++ resources/views/pages/about.blade.php
@@ -0,0 +1,6 @@
+<x-layouts.app.header>
+    <section class="page">
+        <h1 class="page-title">{{ __('About') }}</h1>
+        <p class="page-lead">Learn more about {{ config('app.name') }}.</p>
+    </section>
+</x-layouts.app.header>
