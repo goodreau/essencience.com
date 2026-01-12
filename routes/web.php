65c28d17f28628267a009f627528b90f1f3c7d18
Quintessent.one <gene.goodreau@icloud.com>
2026-01-12 00:06:18 -0500

    Add Moku routes to web.php

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\HomeController;
 use App\Http\Controllers\AboutController;
 use App\Http\Controllers\ServicesController;
 use App\Http\Controllers\ContactController;
+use App\Http\Controllers\MokuController;
 
 Route::get('/', HomeController::class)->name('home');
 Route::get('/about', AboutController::class)->name('about');
@@ -14,5 +15,13 @@ Route::get('/contact', ContactController::class)->name('contact');
 Route::view('dashboard', 'dashboard')
     ->middleware(['auth', 'verified'])
     ->name('dashboard');
+
+// Moku:Go Interface (public for now, can add auth later)
+Route::prefix('moku')->group(function () {
+    Route::get('/', [MokuController::class, 'index'])->name('moku.dashboard');
+    Route::get('/devices', [MokuController::class, 'devices'])->name('moku.devices');
+    Route::get('/device-info', [MokuController::class, 'deviceInfo'])->name('moku.device-info');
+    Route::get('/execute', [MokuController::class, 'executeCommand'])->name('moku.execute');
+});
 
 require __DIR__.'/settings.php';
