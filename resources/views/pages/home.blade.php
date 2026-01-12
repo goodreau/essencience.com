f1e3e36a4983cb3967348c25a1335ce3fcd8acfe
Quintessent.one <gene.goodreau@icloud.com>
2026-01-11 22:21:09 -0500

    Add static Laravel view for Home page (no JS)

---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,6 @@
+<x-layouts.app.header>
+    <section class="page">
+        <h1 class="page-title">{{ __('Home') }}</h1>
+        <p class="page-lead">Welcome to {{ config('app.name') }}.</p>
+    </section>
+</x-layouts.app.header>
