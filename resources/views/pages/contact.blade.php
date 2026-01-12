b3a3a9252da4b17621050963af4fffb4e4dad051
Quintessent.one <gene.goodreau@icloud.com>
2026-01-11 22:21:42 -0500

    Add static Laravel view for Contact page (no JS)

---
+++ resources/views/pages/contact.blade.php
@@ -0,0 +1,6 @@
+<x-layouts.app.header>
+    <section class="page">
+        <h1 class="page-title">{{ __('Contact') }}</h1>
+        <p class="page-lead">Get in touch with {{ config('app.name') }}.</p>
+    </section>
+</x-layouts.app.header>
